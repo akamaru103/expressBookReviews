79ed024304a20e8e61b25aa8ab6f5802bd9bcac4
akamaru103 <shibainu103@gmail.com>
2025-06-11 02:34:30 -0400

    Task 6 completed

--- final_project/index.js
+++ final_project/index.js
@@ -4,6 +4,25 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
+
+let users = []
+
+// Check if a user with the given username already exists
+const doesExist = (username) => {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
 const app = express();
 
 app.use(express.json());
@@ -28,6 +47,35 @@ app.use("/customer/auth/*", function auth(req,res,next){
     } else {
         return res.status(403).json({ message: "User not logged in" });
     }
+});
+
+
+
+// Register a new user
+app.post("/register", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    let errorDetails ="";
+
+    // Check if both username and password are provided
+    if (username && password) {
+        // Check if the user does not already exist
+        if (!doesExist(username)) {
+            // Add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can log in."});
+        } else {
+            return res.status(404).json({message: "User already exists."});
+        }
+    } else if (!username && !password){
+        errorDetails="No username or password provided.";
+    } else if (!username && password){
+        errorDetails="No username provided.";
+    } else if (!password && username){
+        errorDetails="No password provided.";
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user. " + errorDetails});
 });
  
 
