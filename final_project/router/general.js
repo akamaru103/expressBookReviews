4d8b405d333860ad2acfa3a6585fc444f1b9dc7f
akamaru103 <shibainu103@gmail.com>
2025-06-11 03:20:34 -0400

    Task 7 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,9 +5,47 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
+// TASK 6.1 - Complete the code for registering a new user
+// Check if a user with the given username already exists
+const doesExist = (username) => {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // TASK 6.2 - Complete the code for registering a new user
+    const username = req.body.username;
+    const password = req.body.password;
+    let errorDetails ="";
+
+    // Check if both username and password are provided
+    if (username && password) {
+        // Check if the user does not already exist
+        if (!doesExist(username)) {
+            // Add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can log in."});
+        } else {
+            return res.status(404).json({message: "User already exists."});
+        }
+    } else if (!username && !password){
+        errorDetails="No username or password provided.";
+    } else if (!username && password){
+        errorDetails="No username provided.";
+    } else if (!password && username){
+        errorDetails="No password provided.";
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user. " + errorDetails});
 });
 
 // Get the book list available in the shop
